541e12eafae416aa2e3e071e97fe6e2587bdbba9
soroushamdg <soroushmpour@gmail.com>
2022-09-30 19:53:46 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Soro on 2022-09-30.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
